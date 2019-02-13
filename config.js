a1f051cbd7db1c1d55b61650ff14db6a28eb91d2
ikechukwu <ikechukwu.madu1@gmail.com>
2019-02-13 16:54:21 +0100

    Added config file for azure service

---
+++ config.js
@@ -0,0 +1,16 @@
+// config/db.js
+module.exports = function () {
+  return {
+    sessionSecret: '$nMA9248pqZB',
+    maxAge: 120000,
+    secret: 'DoIZOV54qVdnY3tvLW5Z4%^sCSS^P',
+    tokenExpiresIn: 86400,
+    mailExpiresin: 3600,
+    db: 'mongodb://imadushop:passme123@ds054118.mlab.com:54118/cakes-and-cream',
+    clientId: '556217923552-dreaqnat1f3j23984uqe49v06njh47p1.apps.googleusercontent.com',
+    clientSecret: 'uMK9AaNnhORMgmyp7IimdTSm',
+    redirectUri: 'https://developers.google.com/oauthplayground',
+    refresh_token: '1/yANgitwN-_E2eVwn_XmuPU7cBtVMBRACnxKvkSEuW20',
+    accessToken: 'ya29.GlsOBjqPJuQ9T-uvQ7pMew8HxTdlRCOYIm_IoZjXHhj_rH5xDrRKLJluKkhQe0SPOqgHH5drFFZ1d4_zwaxSUzjTFxeAi0RVShJbPxtQ2XblqJlPlNaafnhmNgBS',
+  };
+};
